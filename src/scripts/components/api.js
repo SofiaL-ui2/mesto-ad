fc00f5040c1a3330dacea8a7dc371b2eb1e11fb6
SofiaL-ui2 <sofialyukova2905@gmail.com>
2026-01-18 14:43:28 +0500

    Update api.js

--- src/scripts/components/api.js
+++ src/scripts/components/api.js
@@ -1,1 +1,7 @@
-
+const config = {
+  baseUrl: "https://mesto.nomoreparties.co/v1/{{ apf-cohort-202 }}",
+  headers: {
+    authorization: "{{ 44b7f448-6bcd-4094-a710-d8bfbfc80c1c }}",
+    "Content-Type": "application/json",
+  },
+};
